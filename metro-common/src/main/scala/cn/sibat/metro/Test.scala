f36f00acbb63fe50020a32ff21c697b0f48fa30b
kong <konvish@qq.com>
2017-04-17 09:40:26 +0800

    添加包名

---
+++ metro-common/src/main/scala/cn/sibat/metro/Test.scala
@@ -0,0 +1,8 @@
+package cn.sibat.metro
+
+/**
+  * Created by kong on 2017/4/17.
+  */
+object Test {
+
+}
---
+++ taxi-common/src/main/scala/cn/sibat/taxi/Test.scala
@@ -0,0 +1,8 @@
+package cn.sibat.taxi
+
+/**
+  * Created by kong on 2017/4/17.
+  */
+object Test {
+
+}
