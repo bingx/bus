90fe1a01bde46a9d6aeb027c0a46bfca1cd8992e
wing1995 <1130384613@qq.com>
2017-05-09 22:33:52 +0800

    添加时间戳与时间字符串的转换

--- metro-common/src/test/scala/cn/sibat/metro/demoTest.scala
+++ metro-common/src/test/scala/cn/sibat/metro/demoTest.scala
@@ -1,35 +1,44 @@
 package cn.sibat.metro
 
 import org.apache.spark.sql.functions.{col, _}
-import org.apache.spark.sql.{Row, SQLContext, SparkSession}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.DateType
 
 /**
   * Created by wing1995 on 2017/5/8.
   */
-object demoTest extends App {
-  val spark = SparkSession
-    .builder()
-    .config("spark.sql.warehouse.dir", "file:/file:E:/bus")
-    .appName("Spark SQL Test")
-    .master("local[*]")
-    .getOrCreate()
+object demoTest {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder()
+      .config("spark.sql.warehouse.dir", "file:/file:E:/bus")
+      .appName("Spark SQL Test")
+      .master("local[*]")
+      .getOrCreate()
 
-  import spark.implicits._
+    import spark.implicits._
 
-  val SZT = Seq(
-    (268017, "123456", "地铁三号线", "None"),
-    (268018, "456789", "地铁二号线", "车公庙"),
-    (268018, "234567", "地铁二号线", "车公庙"),
-    (268019, "123456", "地铁二号线", "None")).toDF("siteId", "recordCode", "routeName", "siteName")
+    val SZT = Seq(
+      (268017, "123456", "2017-01-01 04:21:41", "36233", "地铁三号线", "None"),
+      (268018, "456789", "2017-01-01 14:50:41", "36233", "地铁二号线", "车公庙"),
+      (268018, "234567", "2017-01-02 04:21:41", "36222", "地铁二号线", "车公庙"),
+      (268019, "123456", "2017-01-02 03:21:41", "36222", "地铁二号线", "None"))
+      .toDF("siteId", "recordCode", "cardTime", "cardCode", "routeName", "siteName")
 
-  val station = Seq(
-    (268017, "地铁二号线", "深圳北"),
-    (268018, "地铁二号线", "车公庙"),
-    (268019, "地铁二号线", "西丽")
-  ).toDF("siteId", "routeNameStatic", "siteNameStatic")
+    val station = Seq(
+      (268017, "地铁二号线", "深圳北"),
+      (268018, "地铁二号线", "车公庙"),
+      (268019, "地铁二号线", "西丽")
+    ).toDF("siteId", "routeNameStatic", "siteNameStatic")
 
-  val result = SZT.join(station, Seq("siteId")) //如果是unoin将只是添加记录，相当于insert
-  result.withColumn("routeName", when(col("routeName") =!= col("routeNameStatic"), col("routeNameStatic")).otherwise(col("routeName")))
-    .withColumn("siteName", when(col("siteName") === "None", col("siteNameStatic")).otherwise(col("siteName")))
-    .select("siteId", "recordCode", "routeName", "siteName").show()
+    var result = SZT.join(station, Seq("siteId")) //join not add union(insert records)
+      .withColumn("routeName", when(col("routeName") =!= col("routeNameStatic"), col("routeNameStatic")).otherwise(col("routeName")))
+      .withColumn("siteName", when(col("siteName") === "None", col("siteNameStatic")).otherwise(col("siteName")))
+      .select("siteId", "recordCode", "cardTime", "cardCode", "routeName", "siteName")
+
+    val ts = unix_timestamp($"cardTime", "yyyy-MM-dd HH:mm:ss").cast("timestamp")
+    result = result
+      .withColumn("dateStamp", ts)
+      .withColumn("dateColumn", col("dateStamp").cast(DateType))
+  }
 }
